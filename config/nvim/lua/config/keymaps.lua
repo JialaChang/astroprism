2d14232d41401a52851cbd248c2cdc920e881fd6
JialaChang <jiala20061218@gmail.com>
2026-08-25 18:37:43 +0800

    feat(nvim): add Neovide configuration and remove unuse scripts

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,16 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", ";;", "<Esc>", { desc = "Exit insert mode" })
+
+if vim.g.neovide then
+  -- font zoom
+  vim.keymap.set({ "n", "v" }, "<C-=>", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1
+  end, { desc = "Increase font scale" })
+  vim.keymap.set({ "n", "v" }, "<C-->", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1
+  end, { desc = "Decrease font scale" })
+  vim.keymap.set({ "n", "v" }, "<C-0>", function()
+    vim.g.neovide_scale_factor = 1.0
+  end, { desc = "Reset font scale" })
+end
